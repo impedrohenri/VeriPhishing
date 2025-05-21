b1f26f9e15af9d9bde08d6b52a3abc6e7b247827
Pedro H. Silva <impedrohenri@gmail.com>
2025-05-21 16:28:22 -0300

    atualiza arquivo de dependências

--- ai/model.py
+++ ai/model.py
@@ -16,7 +16,14 @@ x_treino, x_teste, y_treino, y_teste = train_test_split(x, y , test_size= 0.20, 
 
 # ------------ CatBoost -----------
 
-CatBoost_model = CatBoostClassifier(border_count= 128, depth= 6, iterations= 200, l2_leaf_reg= 3, learning_rate= 0.1)
+CatBoost_model = CatBoostClassifier(
+    border_count= 128, 
+    depth= 6, 
+    iterations= 200, 
+    l2_leaf_reg= 3, 
+    learning_rate= 0.1
+)
+
 CatBoost_model.fit(x_treino, y_treino)
 
 y_pred_CB = CatBoost_model.predict(x_teste)
